fa2eeac28a6b36499737ef296a0afd359e0652f7
SouravA <souravahmedconsultbae@gmail.com>
2023-04-27 10:20:10 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,107 +1,74 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ToDoList from "./ToDoList";
 import './App.css';
 
 
-const getLocalItems=()=>{
-  let list=localStorage.getItem('lists');
-  console.log(list);
-  if(list){
-    return JSON.parse(localStorage.getItem('lists'));
-  }
-  else{
-    return [];
-  }
+const getLocalItems = () => {
+  return JSON.parse(localStorage.getItem('lists'));
 }
 
 
-const App =()=> {
+const App = () => {
   const [inputList, setInputList] = useState("");
-  const [items, setItems] = useState([getLocalItems()]);
+  const [items, setItems] = useState(getLocalItems() ?? []);
 
-const itemEvent=(event) => {
-  setInputList(event.target.value);
- 
-}; 
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
 
-const listOfItems=()=>{
-  setItems((oldItems)=>{
-    return[...oldItems,inputList];
-  });
-  setInputList("");
-};
+  };
 
-const deleteItems=(id)=>{
-       console.log('deleted');
+  const syncToLocal = (items) => {
+    localStorage.setItem('lists', JSON.stringify(items));
+  }
 
-       setItems((oldItems)=>{
-        return oldItems.filter((arrElem,index)=>{
-          return index!==id;
-        })
-       })
-   };
+  const addToTodo = () => {
+    const newList = [...items, inputList];
+    setItems(newList);
+    setInputList("");
+    syncToLocal(newList);
+  };
 
-  //  const getlocalitems=()=>{
-  //   let list=localStorage.getItem('lists');
-  //   console.log(list);
-  //   if(list){
-  //     return JSON.parse(localStorage.getItem('lists'));
-  //   }
-  //   else{
-  //     return [];
-  //   }
-  //  }
+  const deleteItems = (idx) => {
+    const newList = [...items];
+    newList.splice(idx, 1);
+    setItems(newList);
+    syncToLocal(newList);
+  };
 
-
-  //  useEffect(() => {
-  //   const storedTodos = JSON.parse(localStorage.getItem(todos));
-  //   console.log('storedTodos',storedTodos)
-  //   if (storedTodos) {
-  //     setTodos(storedTodos);
-  //   }
-  // }, []);
-
-  useEffect(() => {
-    localStorage.setItem("lists", JSON.stringify(items))
-  }, [items]);
-
-   
+  
 
   return (
     <>
-  <div class="main_div">
-  <div class="center_div"> 
-  <br/>
-  <h1>ToDo List</h1>
-  <br/>
-  <input type='text' placeholder="add item" 
-  value={inputList}
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type='text' placeholder="add item"
+            value={inputList}
+            onChange={itemEvent}></input>
 
-  onChange={itemEvent}></input>
-  
-  <button onClick={listOfItems}> + </button>
-  
-  <ol>
-  
-  
-{
-  items.map( (itemval,index) => {
-    // return <li> {npm startitemval} </li>
-    return (
-       <ToDoList 
-       key={index}
-      id={index}
-     text={itemval}
-     onSelect={deleteItems}
-     
-     />
-    );
-  } )}
+          <button onClick={addToTodo}> + </button>
 
-  </ol>
-  </div>  
-  </div>
-  </>
+          <ol>
+
+
+            {
+              items.map((itemval, index) => {
+                return (
+                  <ToDoList
+                    key={index}
+                    id={index}
+                    text={itemval}
+                    onSelect={deleteItems}
+                  />
+                );
+              })}
+
+          </ol>
+        </div>
+      </div>
+    </>
   );
 };
 
